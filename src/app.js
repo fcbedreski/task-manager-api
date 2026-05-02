c724353be6360151969f4dd002a6b6d6e9b9d1a9
fcbedreski <fbedreski@gmail.com>
2026-05-02 00:10:08 -0300

    feat: create a router for tasks inside app

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const jwt = require('jsonwebtoken');
 const secretKey = process.env.JWT_SECRET;
+const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use('/users', userRoutes);
+app.use('/tasks', taskRoutes);
 
 app.get('/', (req, res) => {
     res.send('API is running');
