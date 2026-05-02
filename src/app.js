389f5885860e5ac0d8537f1f7bf3859c1c251517
fcbedreski <fbedreski@gmail.com>
2026-05-02 00:11:49 -0300

    refactor: group app constants by similarity for better organization

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
-const userRoutes = require('./routes/userRoutes');
+const app = express();
+
 const jwt = require('jsonwebtoken');
 const secretKey = process.env.JWT_SECRET;
-const taskRoutes = require('./routes/taskRoutes');
 
-const app = express();
+const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 app.use(express.json());
 app.use('/users', userRoutes);
