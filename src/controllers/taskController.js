c9f6904426f4a6b511662089238cbb22fa29ae92
fcbedreski <fbedreski@gmail.com>
2026-05-03 17:45:21 -0300

    feat: create get all action in task controller

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -14,3 +14,16 @@ exports.create = async (req, res) => {
         res.status(400).json({ error: err.message });
     }
 }
+
+exports.getAll = async (req, res) => {
+
+    const userId = req.userId;
+
+    try {
+        const tasks = await taskService.getTasksByUser(userId);
+        res.json(tasks);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
