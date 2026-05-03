99c74910025ba95decb6a57781f4cf05b3ef5f1c
fcbedreski <fbedreski@gmail.com>
2026-05-03 17:46:48 -0300

    feat: create the get all tasks route

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -5,5 +5,6 @@ const taskController = require('../controllers/taskController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/', authMiddleware, taskController.create);
+router.get('/', authMiddleware, taskController.getAll); 
 
 module.exports = router; 
