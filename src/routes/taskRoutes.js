f00d82a704872708b42388fe2119228d40ebcea2
fcbedreski <fbedreski@gmail.com>
2026-05-06 10:15:04 -0300

    feat: create a route for delete task action

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -7,5 +7,6 @@ const authMiddleware = require('../middlewares/authMiddleware');
 router.post('/', authMiddleware, taskController.create);
 router.get('/', authMiddleware, taskController.getAll); 
 router.put('/:id', authMiddleware, taskController.update);
+router.delete('/:id', authMiddleware, taskController.delete);
 
 module.exports = router; 
