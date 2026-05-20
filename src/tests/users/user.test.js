ae8aa2230690e438984e7cd76a318f795c21577e
fcbedreski <fbedreski@gmail.com>
2026-05-20 14:39:10 -0300

    test: verify duplicated email in register feature

--- src/tests/users/user.test.js
+++ src/tests/users/user.test.js
@@ -3,6 +3,8 @@ const app = require('../../app');
 
 const userEmail = 'user@test.com';
 const userPassword = 'strongpassword';
+const duplicatedEmail = 'duplicated@email.com';
+const duplicatedPassword = 'password';
 
 describe('Users', () => {
 
@@ -36,5 +38,28 @@ describe('Users', () => {
         expect(response.body.success).toBe(true);
 
         expect(response.body.data).toHaveProperty('token');
+    });
+
+    it('should not register duplicated email', async () => {
+
+        await request(app)
+            .post('/users/register')
+            .send({
+                email: duplicatedEmail,
+                password: duplicatedPassword
+            });
+
+        const response = await request(app)
+            .post('/users/register')
+            .send({
+                email: duplicatedEmail,
+                password: duplicatedPassword
+            });
+
+        expect(response.statusCode).toBe(409);
+
+        expect(response.body.sucess).toBe(false);
+
+        expect(response.body.error).toBe('Email already registered.');
     });
 });
